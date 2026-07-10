9bb00e7e0a7518139f6a1b992dcf96ebecf165e8
vmobmx <tivanew2007@gmail.com>
2026-07-10 17:45:42 +0300

    Make Block4/Task1

---
+++ Block4/Task1.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace task4_1
+{
+    internal class Task0
+    {
+        private static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            for (int i = 1; i <= 20; ++i)
+            {
+                numbers.Add(i);
+            }
+            
+            var selectedNumbers = (from n in numbers
+                                    where n % 2 == 0
+                                    orderby n descending 
+                                    
+                                    select n).Take(5);
+
+            foreach (var a in selectedNumbers)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    } 
+}
+
